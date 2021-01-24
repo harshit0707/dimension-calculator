1bf5d5d7bd94ccd0f8e3d660e18b762175248a97
Harshit Agrawal <harshit.agrawal@diligenta.co.uk>
2021-01-24 21:35:16 +0530

    added fading in animation for components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('elementAnimation', [
+      state('initial', style({
+        transform: 'translateY(10%)', opacity: 0
+      })),
+      state('final', style({
+        transform: 'translateY(0%)', opacity: 1
+      })),
+      transition('initial => final', [
+        animate('1s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+
+  inFocus: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.inFocus = true;
+    }, 100)
   }
 
 }
